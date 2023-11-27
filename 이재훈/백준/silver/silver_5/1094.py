4fe570b4a07b804529c6d412307e2a22c9433cf2
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2023-11-27 20:22:28 +0900

    Delete 이재훈 directory

--- 이재훈/백준/silver/silver_5/1094.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-length = int(sys.stdin.readline())
-
-print(sum(list(map(int, format(length,'b')))))
--- 이재훈/백준/silver/silver_5/1181.py
+++
@@ -1,9 +0,0 @@
-import sys
-n = int(sys.stdin.readline())
-arr = set()
-for i in range(n):
-    arr.add(sys.stdin.readline().strip())
-
-arr = list(sorted(arr, key=lambda x:(len(x), x)))
-for i in arr:
-    print(i)
