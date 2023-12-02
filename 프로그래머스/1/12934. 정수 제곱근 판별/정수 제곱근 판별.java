b44b642253540c4104bba010be64078fa40e02ae
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2023-12-02 22:35:39 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        
+        long root = (long) Math.sqrt(n);
+        if (root * root == n) {
+            return (root + 1) * (root + 1);
+        }
+        
+        return -1;
+    }
+}
