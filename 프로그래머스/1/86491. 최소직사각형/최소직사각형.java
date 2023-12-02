e92ed30f2b91591bf43dc4e6366af449e0b73a42
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2023-12-02 23:10:31 +0900

    [level 1] Title: 최소직사각형, Time: 2.14 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,25 +2,33 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
-        List<Integer> width = new ArrayList<>();
-        List<Integer> height = new ArrayList<>();
+        int width = -1;
+        int height = -1;
         
         for (int i = 0; i < sizes.length; i++) {
-            if (sizes[i][1] > sizes[i][0]) {
-                int tmp = sizes[i][1];
-                sizes[i][1] = sizes[i][0];
-                sizes[i][0] = tmp;
-                width.add(sizes[i][0]);
-                height.add(sizes[i][1]);
-            }
-            width.add(sizes[i][0]);
-                height.add(sizes[i][1]);
+            width = Math.max(width, Math.max(sizes[i][0], sizes[i][1]));
+            height = Math.max(height, Math.min(sizes[i][0], sizes[i][1]));
         }
         
-        int maxWidth = width.stream().mapToInt(Integer::intValue).max().getAsInt();
-        int maxHeight = height.stream().mapToInt(Integer::intValue).max().getAsInt();
+        return width * height;
+//         List<Integer> width = new ArrayList<>();
+//         List<Integer> height = new ArrayList<>();
         
-        return maxWidth * maxHeight;
+//         for (int i = 0; i < sizes.length; i++) {
+//             if (sizes[i][1] > sizes[i][0]) {
+//                 int tmp = sizes[i][1];
+//                 sizes[i][1] = sizes[i][0];
+//                 sizes[i][0] = tmp;
+//                 width.add(sizes[i][0]);
+//                 height.add(sizes[i][1]);
+//             }
+//             width.add(sizes[i][0]);
+//                 height.add(sizes[i][1]);
+//         }
+        
+//         int maxWidth = width.stream().mapToInt(Integer::intValue).max().getAsInt();
+//         int maxHeight = height.stream().mapToInt(Integer::intValue).max().getAsInt();
+        
+//         return maxWidth * maxHeight;
     }
 }
