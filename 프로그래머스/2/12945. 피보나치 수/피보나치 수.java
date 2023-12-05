7ea50a923002bb5e6d0a981dd627e09275e93beb
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2023-12-05 13:33:50 +0900

    [level 2] Title: 피보나치 수, Time: 2.58 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -6,11 +6,11 @@ class Solution {
         arr[1] = 1;
         
         for (int i = 2; i <= n; i++) {
-            arr[i] = arr[i - 1] + arr[i - 2];
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
         }
         
         answer = arr[n];
         
-        return answer % 1234567;
+        return answer;
     }
 }
