105ba274ae682097af0e1f1f09cc5ef4dd323530
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2024-04-10 17:15:23 +0900

    [level 1] Title: 모의고사, Time: 3.14 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,26 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        List<Integer> answer = new ArrayList<>();
-        int[] one = {1, 2, 3, 4, 5};
-        int[] two = {2, 1, 2, 3, 2 , 4, 2, 5};
-        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] cnt = new int[3];
+        int[] first = {1, 2, 3, 4, 5};
+        int[] second = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] corrects = new int[3];
         
-        int max = -1;
-        for(int i=0; i<answers.length; i++) {
-            if(answers[i] == one[i%one.length]) cnt[0]++;
-            if(answers[i] == two[i%two.length]) cnt[1]++;
-            if(answers[i] == three[i%three.length]) cnt[2]++;
-        }
-        for(int i=0; i<cnt.length; i++) {
-            if(max < cnt[i]) max = cnt[i];
-        }
-        
-        for(int i=0; i<3; i++) {
-            if(max == cnt[i]) answer.add(i+1);
+        for (int i = 0; i < answers.length; i++) {
+            if (first[i % first.length] == answers[i]) {
+                corrects[0]++;
+            }
+            if (second[i % second.length] == answers[i]) {
+                corrects[1]++;
+            }
+            if (third[i % third.length] == answers[i]) {
+                corrects[2]++;
+            }
         }
         
-        return answer.stream().mapToInt(i -> i).toArray();
+        final int maxCorrect = Arrays.stream(corrects)
+            .max()
+            .getAsInt();
+        
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < corrects.length; i++) {
+            if (maxCorrect == corrects[i]) {
+                result.add(i + 1);
+            }
+        }
+        
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
