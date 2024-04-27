3cba48d89637526a8e43143e64d2e2f7668e648d
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2024-04-27 19:53:29 +0900

    [level 2] Title: 주식가격, Time: 9.26 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,36 +2,17 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        List<Integer> answer = new ArrayList<>();
-//         Queue<Integer> queue = new LinkedList<>();
+        int[] answer = new int[prices.length];
         
-//         for(int i : prices) queue.offer(i);
-        
-//         int size = queue.size();
-        
-//         for(int i=0; i<size; i++) {
-//             int num = queue.poll();
-//             int cnt = 0;
-//             for(int j=i+1; j<size; j++) {
-//                 cnt++;
-//                 if(num > prices[j]) {
-//                     break;
-//                 }
-//             }
-//             answer.add(cnt);
-//         }
-        
-        for(int i=0; i<prices.length; i++) {
-            int cnt = 0;
-            for(int j=i+1; j<prices.length; j++) {
-                cnt++;
-                if(prices[i] > prices[j]) {
-                    break;
-                }
+        for (int i = 0; i < prices.length; i++) {
+            int count = 0;
+            for (int j = i + 1; j < prices.length; j++) {
+                count++;
+                if (prices[i] > prices[j]) break;
             }
-            answer.add(cnt);
+            answer[i] = count;
         }
         
-        return answer.stream().mapToInt(x->x).toArray();
+        return answer;
     }
 }
