2c838ac28fdfd30351fbec02ede8cc0b8868526b
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2024-05-29 00:56:01 +0900

    [level 1] Title: 신고 결과 받기, Time: 3.18 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,73 +2,39 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    public int[] solution(String[] id_list, String[] report, int k) {
-        Map<String, Integer> orders = new HashMap<>();
-        Map<String, Integer> counts = new LinkedHashMap<>();
-        Map<String, Integer> result = new LinkedHashMap<>();
-        List<Person> persons = new ArrayList<>();
+    public int[] solution(String[] ids, String[] reports, int k) {
+        Map<String, Set<String>> map = new HashMap<>();
+        Map<String, Integer> counts = new HashMap<>();
+        Map<String, Integer> answer = new HashMap<>();
         
-        for (int i = 0; i < id_list.length; i++) {
-            Person person = new Person(id_list[i]);
-            orders.put(id_list[i], i);
-            counts.put(id_list[i], 0);
-            result.put(id_list[i], 0);
-            persons.add(person);
+        for (String id : ids) {
+            map.put(id, new HashSet<>());
+            answer.put(id, 0);
         }
         
-        for (int i = 0; i < report.length; i++) {
-            StringTokenizer st = new StringTokenizer(report[i]);
-            String username = st.nextToken();
-            int idx = orders.get(username);
-            Person person = persons.get(idx);
-            person.addReport(st.nextToken());
-        }
-        
-        for (int i = 0; i < persons.size(); i++) {
-            Set<String> sets = persons.get(i).getReports();
-            Iterator iter = sets.iterator();
-            while(iter.hasNext()) {
-                String str = String.valueOf(iter.next());
-                counts.put(str, counts.getOrDefault(str, 0) + 1);
+        for (String report : reports) {
+            String splits[] = report.split("\\s");
+            if (map.get(splits[0]).add(splits[1])) {
+                counts.put(splits[1], counts.getOrDefault(splits[1], 0) + 1);
             }
         }
         
-        for (String key : counts.keySet()) {
-            if (counts.get(key) >= k) {
-                for (int i = 0; i < persons.size(); i++) {
-                    Set<String> sets = persons.get(i).getReports();
-                    if (sets.contains(key)) {
-                        result.put(persons.get(i).getName(), result.getOrDefault(persons.get(i).getName(), 0) + 1);
-                    }
+        for (Map.Entry<String, Set<String>> entry : map.entrySet()) {
+            Set<String> set = entry.getValue();
+            set.stream().forEach(s -> {
+                if (counts.get(s) >= k) {
+                    answer.put(entry.getKey(), answer.getOrDefault(entry.getKey(), 0) + 1);
                 }
-            }
-        }
-        List<Integer> answer = new ArrayList<>();
-        for (String key : result.keySet()) {
-            answer.add(result.get(key));
+            });
         }
         
-        return answer.stream().mapToInt(Integer::intValue).toArray();
-    }
-    
-    class Person {
-        private final String name;
-        private Set<String> reports = new HashSet<>();
-        
-        public Person(final String name) {
-            this.name = name;
+        List<Integer> numbers = new ArrayList<>();
+        for (String id : ids) {
+            numbers.add(answer.get(id));
         }
         
-        public String getName() {
-            return name;
-        }
-        
-        public Set<String> getReports() {
-            return reports;
-        }
-        
-        public void addReport(final String name) {
-            reports.add(name);
-        }
+        return numbers.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
     }
 }
