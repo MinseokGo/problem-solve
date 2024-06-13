125f94e633eab6d3dfaf5588c0043ecf7eb6c876
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2024-06-13 13:05:12 +0900

    [level 1] Title: 폰켓몬, Time: 2.21 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        int select = nums.length/2;
-        HashMap<String, Boolean> map = new  HashMap<>();
+        Set<Integer> set = new HashSet<>();
         
-        for(int i : nums) map.put(String.valueOf(i), true);
+        for (int num : nums) {
+            set.add(num);
+        }
         
-        if(map.size() > select) return select;
-        else answer = map.size();
-        
-        return answer;
+        if (nums.length / 2 > set.size()) {
+            return set.size();
+        } else {
+            return nums.length / 2;
+        }
     }
 }
