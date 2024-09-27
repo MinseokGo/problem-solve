6a3f9e0bcf1cab63bb43ee663ac916d3e96edc30
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2024-09-27 18:45:47 +0900

    [level 2] Title: 테이블 해시 함수, Time: 14.45 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
-        int answer = 1;
+        int answer = 0;
         
         Arrays.sort(data, (number1, number2) -> {
             if (number1[col - 1] > number2[col - 1]) {
@@ -19,11 +19,6 @@ class Solution {
             int sum = 0;
             for (int j = 0; j < row.length; j++) {
                 sum += (row[j] % (i + 1));
-            }
-            
-            if (i == row_begin - 1) {
-                answer = sum;
-                continue;
             }
             answer ^= sum;
         }
