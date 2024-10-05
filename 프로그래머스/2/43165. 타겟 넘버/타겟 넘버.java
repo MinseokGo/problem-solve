c806b0c33848692e78e08898d2668e5e0cc48e5e
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2024-10-05 18:40:01 +0900

    [level 2] Title: 타겟 넘버, Time: 9.14 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,18 +1,20 @@
 class Solution {
+    
     static int answer = 0;
+
     public int solution(int[] numbers, int target) {
         dfs(numbers, 0, target, 0);
-        
         return answer;
     }
-    static void dfs(int[] numbers, int depth, int target, int sum) {
-        if(depth == numbers.length) {
-            if(sum == target) {
+    
+    private void dfs(int[] numbers, int index, int target, int sum) {
+        if (index == numbers.length) {
+            if (sum == target) {
                 answer++;
             }
         } else {
-            dfs(numbers, depth + 1, target, sum + numbers[depth]);
-            dfs(numbers, depth + 1, target, sum - numbers[depth]);
+            dfs(numbers, index + 1, target, sum - numbers[index]);
+            dfs(numbers, index + 1, target, sum + numbers[index]);
         }
     }
 }
