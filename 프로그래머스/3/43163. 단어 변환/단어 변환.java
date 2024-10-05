8706fef09bbc57af638d17784910e79425853bfd
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2024-10-05 21:45:07 +0900

    [level 3] Title: 단어 변환, Time: 1.17 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,10 +1,13 @@
+import java.util.*;
+
 class Solution {
     
     static int answer = Integer.MAX_VALUE;
     
     public int solution(String begin, String target, String[] words) {
         boolean[] visited = new boolean[words.length];
-        int answer = dfs(words, begin, target, visited, 0);
+        // int answer = dfs(words, begin, target, visited, 0);
+        int answer = bfs(words, begin, target, visited);
         return answer == Integer.MAX_VALUE ? 0 : answer;
     }
     
@@ -24,6 +27,28 @@ class Solution {
         return min;
     }
     
+    private int bfs(String[] words, String begin, String target, boolean[] visited) {
+        int answer = Integer.MAX_VALUE;
+        Queue<Current> queue = new LinkedList<>();
+        queue.add(new Current(begin, 0));
+        
+        while (!queue.isEmpty()) {
+            Current cur = queue.poll();
+            if (cur.word.equals(target)) {
+                answer = cur.count;
+                return answer;
+            }
+            
+            for (int i = 0; i < words.length; i++) {
+                if (!visited[i] && isChangedOneSpell(cur.word, words[i])) {
+                    visited[i] = true;
+                    queue.add(new Current(words[i], cur.count + 1));
+                }
+            }
+        }
+        return answer;
+    }
+    
     private boolean isChangedOneSpell(String str, String target) {
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
@@ -32,5 +57,16 @@ class Solution {
             }
         }
         return count == str.length() - 1;
+    }
+    
+    static class Current {
+        
+        String word;
+        int count;
+        
+        Current(String word, int count) {
+            this.word = word;
+            this.count = count;
+        }
     }
 }
