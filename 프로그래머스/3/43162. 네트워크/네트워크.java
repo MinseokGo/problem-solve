436a767b8d962035320e41cf83c6ba70aedf89cb
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2024-10-05 19:23:43 +0900

    [level 3] Title: 네트워크, Time: 0.63 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,4 +1,7 @@
+import java.util.*;
+
 class Solution {
+    
     public int solution(int n, int[][] computers) {
         int answer = 0;
         boolean[] visited = new boolean[n];
@@ -6,7 +9,8 @@ class Solution {
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 answer++;
-                dfs(computers, visited, i);
+                // dfs(computers, visited, i);
+                bfs(computers, visited, i);
             }
         }
         
@@ -19,6 +23,23 @@ class Solution {
         for (int i = 0; i < computers.length; i++) {
             if (i != index && !visited[i] && computers[index][i] == 1) {
                 dfs(computers, visited, i);
+            }
+        }
+    }
+    
+    private void bfs(int[][] computers, boolean[] visited, int index) {
+        visited[index] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(index);
+        
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            int[] computer = computers[current];
+            for (int i = 0; i < computer.length; i++) {
+                if (i != current && !visited[i] && computer[i] == 1) {
+                    queue.add(i);
+                    visited[i] = true;
+                }
             }
         }
     }
