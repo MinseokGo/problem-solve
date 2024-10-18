2c4effb547c2913296cef8c3fa330a11573a4384
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2024-10-18 22:32:06 +0900

    [level 2] Title: 피로도, Time: 4.68 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,23 +2,22 @@ import java.util.*;
 
 class Solution {
     
-    static boolean visited[];
     static int answer = 0;
 
     public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
-        dfs(k, dungeons, 0);
+        boolean[] visited = new boolean[dungeons.length];
+        dfs(k, dungeons, visited, 0);
         return answer;
     }
     
-    public static void dfs(int k, int[][] dungeons, int cnt){
-        for(int i = 0; i < dungeons.length; i++){
-            if(!visited[i] && dungeons[i][0] <= k){
+    private void dfs(int hp, int[][] dungeons, boolean[] visited, int count) {
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && hp >= dungeons[i][0]) {
                 visited[i] = true;
-                dfs(k - dungeons[i][1], dungeons, cnt + 1);
+                dfs(hp - dungeons[i][1], dungeons, visited, count + 1);
                 visited[i] = false;
             }
         }
-        answer = Math.max(answer, cnt);
+        answer = Math.max(answer, count);
     }
 }
