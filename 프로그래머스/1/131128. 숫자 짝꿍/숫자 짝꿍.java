520cbb2c6af1d4a78f779d7538d65ab21af89473
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2025-03-27 11:22:13 +0900

    [level 1] Title: 숫자 짝꿍, Time: 88.83 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -3,36 +3,33 @@ import java.util.*;
 class Solution {
     
     public String solution(String X, String Y) {
-        Map<Character, Integer> map = new HashMap<>();
         StringBuilder sb = new StringBuilder();
-        
-        for (char ch : Y.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1); 
-        }
+        int[] xCount =  new int[10];
+        int[] yCount =  new int[10];
         
         for (char ch : X.toCharArray()) {
-            if (map.containsKey(ch)) {
-                map.put(ch, map.get(ch) - 1);
-                sb.append(ch);
-                if (map.get(ch) == 0) {
-                    map.remove(ch);
-                }
+            xCount[ch - '0'] += 1;
+        }
+        
+        for (char ch : Y.toCharArray()) {
+            yCount[ch - '0'] += 1;
+        }
+        
+        for (int i = 9; i >= 0; i--) {
+            int max = Math.min(xCount[i], yCount[i]);
+            
+            while (max > 0) {
+                sb.append(i);
+                max -= 1;
             }
         }
         
-        char[] chars = sb.toString().toCharArray();
-        Arrays.sort(chars);
+        String answer = sb.toString();
         
-        StringBuilder sortedSb = new StringBuilder();
-        for (int i = 0; i < chars.length; i++) {
-            sortedSb.append(chars[i]);
-        }
-        
-        String answer = sortedSb.reverse().toString();
-        
-        if (answer.length() == 0) {
+        if (answer.isEmpty()) {
             return "-1";
-        } else if (answer.startsWith("0")) {
+        }
+        if (answer.startsWith("0")) {
             return "0";
         }
         
