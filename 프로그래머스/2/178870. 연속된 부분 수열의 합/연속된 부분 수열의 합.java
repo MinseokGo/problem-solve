e24bbde2e08833672af6e1c097fa2f2de15722e0
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2025-05-22 20:46:49 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 14.61 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -3,32 +3,24 @@ import java.util.*;
 class Solution {
     
     public int[] solution(int[] sequence, int k) {
-        Deque<Integer> deque = new ArrayDeque<>();
         int[] answer = new int[2];
         answer[1] = Integer.MAX_VALUE;
         
         int sum = 0;
+        int left = 0;
+        int right = 0;
         for (int i = 0; i < sequence.length; i++) {
-            int number = sequence[i];
-            sum += number;
-            deque.addLast(i);
-            if (sum > k) {
-                while (sum > k) {
-                    sum -= sequence[deque.pollFirst()];
-                    if (sum == k) {
-                        int len = deque.peekLast() - deque.peekFirst();
-                        if (len < answer[1] - answer[0]) {
-                            answer[0] = deque.peekFirst();
-                            answer[1] = deque.peekLast();
-                        }
-                        break;
-                    }
-                }
-            } else if (sum == k) {
-                int len = deque.peekLast() - deque.peekFirst();
-                if (len < answer[1] - answer[0]) {
-                        answer[0] = deque.peekFirst();
-                        answer[1] = deque.peekLast();
+            sum += sequence[i];
+            while (sum > k) {
+                sum -= sequence[left];
+                left += 1;
+            }
+            
+            if (sum == k) {
+                right = i;
+                if (right - left < answer[1] - answer[0]) {
+                    answer[0] = left;
+                    answer[1] = right;
                 }
             }
         }
